ff51f95ebb525be8fa05137f5c8cb368ab67db5c
felipehw <felipehw@gmail.com>
2023-06-27 14:00:57 +0000

    Added react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import '@fontsource/roboto/700.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 import { routes } from './routes/routes.tsx';
 
@@ -13,5 +15,6 @@ const router = createBrowserRouter(routes);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <ToastContainer position="top-center" />
     </React.StrictMode>
 );
